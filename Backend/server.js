862d0d045c1bf2aec374e6b760c3350ba0d55805
r0cKe <rsritiksabhrwal123@mail.com>
2022-12-29 21:09:07 +0530

    initial commit

--- Backend/server.js
+++ Backend/server.js
@@ -57,7 +57,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
 	pingTimeout: 60000,
 	cors: {
-		origin: "https://chatbox-backend.onrender.com",
+		origin: "https://chatbox-dejz.onrender.com",
 	},
 });
 
